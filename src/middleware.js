d6a90bb9f6b56be8a6b3a456587a03ceb3c0f85a
ripred31 <ripred31@gmail.com>
2023-08-17 22:43:23 -0400

    Added middleware

---
+++ src/middleware.js
@@ -0,0 +1,19 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+
+export async function middleware(req) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+  await supabase.auth.getSession()
+
+  console.log('testing middleware')
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if(!user) {
+    return NextResponse.redirect(new URL('https://auth.automatearmy.com/?redirect_url=https://a.automatearmy.com'))
+  }
+
+}
