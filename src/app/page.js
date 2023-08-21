f6f3ef5bf4e3158d3d2e59e6aa6f3ffecd04ace9
ripred31 <ripred31@gmail.com>
2023-08-20 22:44:28 -0400

    Removed some stuff

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,13 @@
 'use client'
-import { useEffect, useState } from 'react';
+// pages/index.js (in your test-app-a project)
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
 export default function Page({ session }) {
-  const [user, setUser] = useState(null);
-  const [userEmail, setUserEmail] = useState(null);
 
-  useEffect(() => {
-    async function fetchUserData() {
-      const supabase = createClientComponentClient({ cookieOptions: { domain: "automatearmy.com", path: "/" } });
+  const supabase = createClientComponentClient( { cookieOptions: {domain: "automatearmy.com", path: "/"} } );
+  const user = supabase.auth.getUser();
 
-      try {
-        const user = await supabase.auth.getUser();
-        setUser(user);
-        setUserEmail(user?.email);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    }
-
-    console.log("Supabase: " + supabase)
-    console.log("Session: " + session)
-    console.log("User: " + user)
-
-    fetchUserData();
-  }, []); // Empty dependency array to run the effect only once
+  const userEmail = user?.email
 
   return (
     <div>
