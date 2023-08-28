3243ab8a929482c26eb24e894b7983ad75d5139f
ripred31 <ripred31@gmail.com>
2023-08-28 10:24:50 -0400

    Test

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,1 +1,9 @@
-<iframe src="https://oohinfo.retool.com/apps/User%20Apps/Dashboard" width="100%" height="800px"></iframe>
+'use client'
+
+export default function Dashboard() {
+    return (
+        <div>
+            <iframe src="https://oohinfo.retool.com/apps/User%20Apps/Dashboard" width="100%" height="800px"></iframe>
+        </div>
+    )
+}
