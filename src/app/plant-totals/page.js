85779bdab916c9142e7d55843b7b8d8d8f4cce37
ripred31 <ripred31@gmail.com>
2023-08-28 20:38:11 -0400

    Added plant-totals page

---
+++ src/app/plant-totals/page.js
@@ -0,0 +1,9 @@
+'use client'
+
+export default function PlantTotals() {
+    return (
+        <div>
+            <iframe src="https://oohinfo.retool.com/apps/User%20Apps/Plant%20Totals" width="100%" height="800px"></iframe>
+        </div>
+    )
+}
