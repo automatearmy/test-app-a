86f53ab788ed9d0db78b5004f0d7d5e92dba5da4
ripred31 <ripred31@gmail.com>
2023-09-04 13:18:07 -0400

    Small changes

--- src/app/components/sidebar.js
+++ src/app/components/sidebar.js
@@ -104,15 +104,15 @@ export default function Sidebar({ session }) {
               {avatarUrl ? (
                 <Image 
                   src={avatarUrl}
-                  width={16}
-                  height={16}
+                  width={32}
+                  height={32}
                   alt="Profile photo"
                   className='rounded-full'
                 />
               ) : (
                 <img
                   className="h-8 w-8 rounded-full bg-gray-50"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  src=""
                   alt=""
                 />
               )}
