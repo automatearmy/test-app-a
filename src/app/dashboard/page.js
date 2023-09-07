465fb796ac1c5dd350f60eb3a2126321b7b51c74
Adam Wattis <adamwattis@Adams-MBP.hsd1.pa.comcast.net>
2023-09-07 15:02:57 -0400

    Remove the useEffect

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,37 +1,33 @@
-import { useEffect, useState } from 'react';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
 import Sidebar from '../components/sidebar'
 
-export default function Dashboard() {
-    const [session, setSession] = useState(null);
-    const [companyId, setCompanyId] = useState(null);
-    
-    useEffect(() => {
-        async function fetchData() {
-            const supabase = createServerComponentClient({ cookies });
-            const { data: { session } } = await supabase.auth.getSession();
-            setSession(session);
+export default async function Dashboard() {
+    const supabase = createServerComponentClient({ cookies })
 
-            if (session) {
-                const { data: profile } = await supabase
-                    .from('profile')
-                    .select('company_id')
-                    .eq('id', session.user.id)
-                    .single();
+    const {
+      data: { session },
+    } = await supabase.auth.getSession()
 
-                setCompanyId(profile ? profile.company_id : null);
-            }
-        }
+    // Fetch the company_id from the profile table where the ID equals to the user ID
+    const { data: profile } = await supabase
+      .from('profile')
+      .select('company_id')
+      .eq('id', session.user.id)
+      .single()
 
-        fetchData();
-    }, []);
+    const companyId = profile ? profile.company_id : null
+    console.log(companyId)
 
     return (
         <div style={{ height: '100vh' }}>
             <div className='flex h-full'>
-                {session && <Sidebar className='h-full' session={session} />}
+                <Sidebar 
+                    className='h-full'
+                    session={session}
+                />
                 {companyId && <iframe src={`https://oohinfo.retool.com/embedded/public/69c1c571-610e-4808-afdc-50fa966cff4a?company_id=${companyId}`} width="100%" height="100%"></iframe>}
             </div>
         </div>
-    );
+    )
 }
