2818240ff687fe6366a89154ae416e5a4e33393c
guilhermebarroso-sys <guibarrosodeoliveira5@gmail.com>
2023-09-28 21:40:09 -0300

    feat: :art: Add colors on the roads/billboards and popup

--- src/app/components/map.js
+++ src/app/components/map.js
@@ -1,42 +1,129 @@
 'use client'
-import ReactMapboxGl, { GeoJSONLayer, Layer, Feature } from 'react-mapbox-gl';
-import * as MapboxGL from 'react-mapbox-gl'
-import roads from "../intensity-score/roads.json";
-import billboards from "../intensity-score/billboards.json";
-
-const token = 'pk.eyJ1IjoienppbWJsZXIiLCJhIjoiY2pqaGhxZDZ3MDBwdTN2bXJiM3E3bm04byJ9.lHf70NPBi012mpkAg47wVw'
-
+import { useState } from 'react';
+import ReactMapboxGl, { GeoJSONLayer, Layer, Feature, Popup, Marker } from 'react-mapbox-gl';
 const Map = ReactMapboxGl({
-  accessToken: token 
-});
+  accessToken: "pk.eyJ1IjoienppbWJsZXIiLCJhIjoiY2pqaGhxZDZ3MDBwdTN2bXJiM3E3bm04byJ9.lHf70NPBi012mpkAg47wVw",
 
-export default function MapComponent() {
+});
+export default function MapComponent({ originCoordinates, roadData, billboardData, pointLayout, symbolLayout, pointPaint, billboardLineColor = {
+  A: 'green',
+  B: 'greenyellow',
+  C: 'yellow',
+  D: 'orange',
+  F: 'red'
+}}) {
+ 
+ 
+  const [billboardPopup, setBillboardPopup] = useState(null) 
+  const [billboardPopupData,setBillboardPopupData] = useState({})
+  const lineLayout = {};
+  
+  const roadLinePaint = {
+    'line-color': {
+      property: 'intensity', 
+      type: 'interval',
+      stops: [
+        [0, "gray"],
+        [20, "gray"],
+        [40, "turquoise"],
+        [60, "magenta"],
+        [80, "red"],
+        [100, "red"]
+      ],
+    },
+    'line-width': 2,
+  };
+  const billboardLinePaint = {
+    'circle-color': {
+      property: 'ScoreUA', 
+      type: 'categorical',
+      stops: [
+        ["A", billboardLineColor.A],
+        ["B", billboardLineColor.B],
+        ["C", billboardLineColor.C],
+        ["D", billboardLineColor.D],
+        ["F", billboardLineColor.F],
+      ],
+    },
+    'circle-radius': 8,
+  };
+  
+  const handleClosePopup = () => {
+    return setBillboardPopup(null)
+  }
   return (
     <>
-      <h5 className="my-8 text-left text-base font-semibold leading-7 text-white">MetricOOH Intensity Score</h5> 
       <Map
         className='rounded-md overflow-hidden' 
-        center={[ -97.524757, 35.473641]}
+        center={originCoordinates}
         style="mapbox://styles/mapbox/streets-v8"
+
       > 
-        <GeoJSONLayer
-          data={billboards}
-          pointLayout={{
-            'text-field': '{place}',
-            'text-font': ['Open Sans Semibold'], 
-            'text-offset': [0, 0.6],
-            'text-anchor': 'top'
-          }}
-          pointPaint={{
-            'text-color': 'green' 
-          }}
+       <GeoJSONLayer
+          data={roadData}
+          lineLayout={lineLayout}
+          linePaint={roadLinePaint}
         />
-        {/*<GeoJSONLayer
-          data={roads}
-          lineLayout={{
-          }}
-          linePaint={{}}
-        />*/}
+        <GeoJSONLayer
+        key={"billboard"}
+        id="billboard"
+        className={"cursor-pointer"}
+        data={billboardData}
+        getCursor={(e) => "pointer"}
+        circlePaint={billboardLinePaint}
+        circleOnClick={(e) => {
+          // setBillboardPopupData(null)
+          // setBillboardPopup(null)
+          const {properties} = e.features[0]
+          setBillboardPopup(e.lngLat)
+          setBillboardPopupData(properties)
+        }}
+      />
+        <Layer  
+        id="billboard-layer"
+        source='billboard'
+        source-layer='original'
+        
+        type={"circle"} paint={billboardLinePaint} />
+        {billboardPopup &&  (
+        <Popup
+          latitude={billboardPopup.lat}
+          longitude={billboardPopup.lng}
+          coordinates={[billboardPopup.lng, billboardPopup.lat]} 
+          closeButton={true}
+          closeOnClick={false}
+          onClose={handleClosePopup}
+          offset={5}
+          >
+          <div className="max-w-xs rounded overflow-hidden">
+            <div className="bg-gray-200 p-2">
+              <div className="font-bold text-xl mb-2 text-right" onClick={() => {
+                return setBillboardPopup(null)
+              }}>X</div>
+              <div className="font-bold  text-center text-xl mb-2">Billboard Details</div>
+            </div>
+            <div className="px-6 py-2">
+              <div className="mb-2">
+                <span className="font-bold" >Score:</span>  <span className='font-bold'>{billboardPopupData.ScoreUA} </span>
+              </div>
+              <div className="mb-2">
+                <span className="font-bold">Revision:</span> {billboardPopupData.Revision}
+              </div>
+              <div className="mb-2">
+                <span className="font-bold">Street View URL:</span>{' '}
+                <a
+                  href={billboardPopupData.StreetViewUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:underline"
+                >
+                  Open Street View
+                </a>
+              </div>
+            </div>
+          </div>
+
+        </Popup>)} 
       </Map>
     </> 
   )
--- src/app/intensity-score/page.jsx
+++ src/app/intensity-score/page.jsx
@@ -2,6 +2,10 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import Sidebar from '../components/sidebar'
 import Map from '../components/map'
+import roads from "../intensity-score/roads.json";
+import billboards from "../intensity-score/billboards.json";
+// import devBillboards from "../intensity-score/devBillboards.json"
+// import devRoads from "../intensity-score/devRoads.json"
 
 export default async function IntensityScore() {
     const supabase = createServerComponentClient({ cookies })
@@ -12,16 +16,38 @@ export default async function IntensityScore() {
 
 
     return (
+      <>
+        <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css' rel='stylesheet' />
         <div style={{ height: '100vh' }}>
-            <div className='flex h-full bg-[#3a435f]'>
+            <div className='flex  h-full bg-[#3a435f]'>
                 <Sidebar 
                     className='h-full'
                     session={session}
                 />
-                <div className='mx-auto max-w-7xl sm:px-6 lg:px-8'>
-                    <Map />
+                <div className='flex flex-col items-center justify-center gap-4 w-full mx-auto'>
+                <h5 className="my-8 text-center font-semibold leading-7 text-white w-full">MetricOOH Intensity Score</h5> 
+                  <div className='w-[95%]'>
+                    <Map roadData={roads} billboardData={billboards} originCoordinates={[ -97.524757, 35.473641]}
+                      pointLayout={{
+                        'text-field': '{place}',
+                        'text-font': ['Open Sans Semibold'], 
+                        'text-offset': [0, 0.6],
+                        'text-anchor': 'top'
+                      }}
+                      symbolLayout={{
+                        'icon-image': 'marker-15', 
+                        'icon-size': 1.5,
+                      }}
+                    />
+                  </div>
+                 
                 </div> 
+    
+     
+                
             </div>
         </div>
+      </>
     )
 }
+
