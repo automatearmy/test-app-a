008d195de48dd9a416705c5d0548546838bd7952
Zach Zimbler <zachzimbler@gmail.com>
2023-10-02 16:32:03 -0600

    Updated the middleware for production usage

--- src/middleware.js
+++ src/middleware.js
@@ -2,17 +2,20 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
+// This will NOT be inlined, because it uses a variable
+  const env = process.env
+
   const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res }, { cookieOptions: {domain: "automatearmy.com", path: "/"} })
+  const supabase = createMiddlewareClient({ req, res }, { cookieOptions: {domain: env.NEXT_PUBLIC_COOKIE_DOMAIN, path: "/"} })
   await supabase.auth.getSession()
 
   const {
     data: { user },
   } = await supabase.auth.getUser()
 
-  /*if(!user) {
-    return NextResponse.redirect(new URL('https://auth.automatearmy.com/?redirect_url=https://a.automatearmy.com/home'))
-  }*/
+  if(!user) {
+    return NextResponse.redirect(new URL(env.NEXT_PUBLIC_AUTH_DOMAIN+'?redirect_url='+env.NEXT_PUBLIC_REDIRECT_DOMAIN+'/home'))
+  }
 
   if(user) {
     return res
