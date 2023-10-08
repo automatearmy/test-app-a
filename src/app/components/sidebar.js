703cf4243619736ad9a5ea99ebef081e765f9d3e
Adam Wattis <adamwattis@Adams-MBP.hsd1.pa.comcast.net>
2023-10-08 18:29:16 -0400

    Update user_id name

--- src/app/components/sidebar.js
+++ src/app/components/sidebar.js
@@ -35,7 +35,7 @@ export default function Sidebar({ session }) {
       let { data, error, status } = await supabase
         .from('profiles')
         .select(`full_name, avatar_url`)
-        .eq('id', user?.id)
+        .eq('user_id', user?.id)
         .single()
 
       if (error && status !== 406) {
